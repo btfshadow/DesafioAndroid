3aca87072d19dab4592a4a7f74abd9901f3302d8
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-08-06 12:21:27 -0300

    Fix Codebeat analysis

--- app/src/main/java/br/com/concretesolutions/desafioandroid/ui/util/ImageUtils.java
+++ app/src/main/java/br/com/concretesolutions/desafioandroid/ui/util/ImageUtils.java
@@ -2,7 +2,6 @@ package br.com.concretesolutions.desafioandroid.ui.util;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 public final class ImageUtils {
@@ -10,15 +9,8 @@ public final class ImageUtils {
     private ImageUtils() {
     }
 
-    public static Bitmap drawableToBitmap (Drawable drawable) {
-        Bitmap bitmap = null;
-
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            if(bitmapDrawable.getBitmap() != null) {
-                return bitmapDrawable.getBitmap();
-            }
-        }
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap;
 
         if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
             bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
