ce2f87a44dd83c5ff1d8a1a98a3ac388deaea166
heitorcolangelo <heitor.colangelo@concrete.com.br>
2017-08-06 12:25:36 -0300

    Codebeat analysis

--- app/src/main/java/br/com/concretesolutions/desafioandroid/ui/util/ImageUtils.java
+++ app/src/main/java/br/com/concretesolutions/desafioandroid/ui/util/ImageUtils.java
@@ -10,17 +10,17 @@ public final class ImageUtils {
     }
 
     public static Bitmap drawableToBitmap(Drawable drawable) {
-        Bitmap bitmap;
-
-        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
-        } else {
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        }
-
+        final Bitmap bitmap = getBitmap(drawable);
         Canvas canvas = new Canvas(bitmap);
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
         drawable.draw(canvas);
         return bitmap;
+    }
+
+    private static Bitmap getBitmap(Drawable drawable) {
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0)
+            return Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        else
+            return Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
     }
 }
